1c7bf6b3cb87573a59e8654d4f55b0e8370ae013
RowanG99 <rowangauld1@gmail.com>
2021-12-10 11:09:12 +0200

    20211210 Initial

--- uctproject/src/app/app.component.ts
+++ uctproject/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { LoginDialogComponent } from './dialogs/login-dialog/login-dialog.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'uctproject';
+export class AppComponent implements OnInit {
+
+  constructor(
+    public dialog: MatDialog,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  dialogLogin() {
+    const dialog = new MatDialogConfig;
+    dialog.disableClose = false;
+    dialog.width = '20rem';
+    dialog.height = 'auto';
+    dialog.data = { add: 'yes' }
+    const dialogReference = this.dialog.open(
+      LoginDialogComponent,
+      dialog
+    );
+  }
+
+
 }
--- uctproject/src/app/app.module.ts
+++ uctproject/src/app/app.module.ts
@@ -5,11 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Angular 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import { LoginDialogComponent } from './dialogs/login-dialog/login-dialog.component';
+
+const angularMaterials = [
+  MatToolbarModule,
+  MatIconModule,
+  MatDialogModule, 
+  MatButtonModule
+]
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginDialogComponent
   ],
   imports: [
+    ...angularMaterials,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
